001827482e4b3f09b97c9e15613cae19fce718c0
DaviAlencar2 <davilt.alencar@gmail.com>
2025-08-30 18:11:52 -0300

    correcao em query

--- js/query.js
+++ js/query.js
@@ -3,5 +3,5 @@
 // Encontrar todos os usuários que são editores ou admins
 db.usuario.find({ nome_perfil: { $in: ["editor", "admin"] } });
 
-// Encontrar usuários com telefones da região 83
+// Encontrar usuários com telefones com ddd 83
 db.usuario.find({ telefone: { $regex: "\\+55 83.*" } });
